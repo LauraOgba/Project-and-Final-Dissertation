5bccc1deecf439bbf64408c0379875d006ad173a
LauraOgba <lauraelohoogba4@gmail.com>
2019-02-18 15:34:50 +0000

    Login completed with profile and dashboard

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -14,13 +14,14 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { ValidateService} from './services/validate.service';
 import { AuthService} from './services/auth.service';
 import { FlashMessagesModule } from 'angular2-flash-messages/module';
+import {AuthGuard} from "./guard/auth.guard";
 
 const appRoutes: Routes = [
   {path:  '', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'profile', component: ProfileComponent}
+  {path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate:[AuthGuard]}
 
 ]
 
@@ -41,7 +42,7 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes),
     FlashMessagesModule
   ],
-  providers: [ValidateService, AuthService],
+  providers: [ValidateService, AuthService,AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
